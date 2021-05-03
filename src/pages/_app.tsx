c1d76dc062b4368dea3c379aa073fecde445418e
Alexander Nakahara <lexnakahara@gmail.com>
2021-05-03 00:05:51 -0300

    Player Context and update state
    Context API from react

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,30 @@
-import '../styles/global.scss'  
+import '../styles/global.scss'
 import styles from '../styles/app.module.scss';
 
 import { Header } from '../components/Header'
 import { Player } from '../components/Player';
+import { playerContext } from '../components/contexts/PlayerContext';
+import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+  }
+
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header></Header>
-        <Component {...pageProps} />
-      </main>
-      <Player/>
-    </div>
+    <playerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header></Header>
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </playerContext.Provider>
   )
 }
 
